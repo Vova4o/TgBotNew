f38231eeb2e2a3e1298300828c0387a3a77099c7
Vladimir <110772568+Vova4o@users.noreply.github.com>
2023-12-29 04:03:53 +0300

    add some comments

--- callback.go
+++ callback.go
@@ -7,12 +7,18 @@ import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 // нужную клаву
 func CallBackAnswer(data string) (string, tgbotapi.InlineKeyboardMarkup) {
 	switch data {
-	case "1":
-		return "Вы выбрали кнопку 1", numericKeyboard
-	case "2":
-		return "Вы выбрали кнопку 2", numericKeyboard
-	case "3":
-		return "Вы выбрали кнопку 3", numericKeyboard
+	case "start":
+		text := "Вы выбрали кнопку 1"
+		return text, numericKeyboard
+	case "letter":
+		text := "Вы выбрали кнопку 2" 
+		return text, numericKeyboard
+	case "number":
+		text := "Вы выбрали кнопку 3" 
+		return text, numericKeyboard
+	case "enterLetter":
+		text := "Вы выбрали кнопку 4" 
+		return text, numericKeyboard
 	}
 	return "Пожалуйста, нажмите *start* для начала работы с ботом", startKeyboard
 }
